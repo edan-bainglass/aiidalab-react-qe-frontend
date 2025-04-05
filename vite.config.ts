aaa46e4c66768215187aada9f908439eaebdcdb6
Edan Bainglass <edan.bainglass@gmail.com>
2025-04-05 16:46:40 +0200

    Update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  base: "/aiidalab/quantum-espresso/",
+});
